b940438421ec31123127f34e4936389ce04c6b83
Sonie Moon <smoon2@uw.edu>
2017-06-11 12:01:06 -0500

    Motified navbar design / css

--- tmp/2k99kpmeb884w.7dybggt7t0tp.php
+++ tmp/2k99kpmeb884w.7dybggt7t0tp.php
@@ -5,10 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <link rel="stylesheet" href="<?= $BASE ?>/css/style.css">
+  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="/css/style.css">
+  <link href='https://fonts.googleapis.com/css?family=Audiowide' rel='stylesheet'>
+  <link rel="stylesheet" href="<?= $BASE ?>/css/style.css">
   </head>
 <body>
     
